108a87576ea74fcb26c18bd3566e031ec2228037
Nakhareid <nakhareid99@gmail.com>
2026-08-29 10:05:27 +0700

    Update data_fetcher.py

--- src/data_fetcher.py
+++ src/data_fetcher.py
@@ -11,9 +11,9 @@ class DataFetcher:
         os.makedirs("data", exist_ok=True)
         cache_file = f"data/{ticker}_{start}_{end}.parquet"
         if os.path.exists(cache_file):
-            print(f"📂 โหลดจาก Cache: {ticker}")
+            print(f" โหลดจาก Cache: {ticker}")
             return pd.read_parquet(cache_file)
-        print(f"🌐 โหลดจาก Yahoo: {ticker}")
+        print(f" โหลดจาก Yahoo: {ticker}")
         df = yf.download(ticker, start=start, end=end, progress=False)
         df.to_parquet(cache_file)
         return df
